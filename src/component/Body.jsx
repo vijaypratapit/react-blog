74ba61bb9fe7e5e5fefc4ec99aa76c64022edc68
vijaypratapit <53234738+vijaypratapit@users.noreply.github.com>
2026-04-03 01:01:50 +0530

    Add test header to Body component

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -5,8 +5,10 @@ import { Outlet } from "react-router";
 import { NavBar } from "./NavBar";
 import "./styles.css";
 const Body = () => {
+  
   return (
     <div>
+      <h1> Test h1/>
       <NavBar />
       <Outlet />
     </div>
